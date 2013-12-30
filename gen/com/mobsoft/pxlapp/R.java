3e507a8c1665ac9b954fe56facbe80f33d251ea6
lambrix <lambrix_s@hotmail.com>
2013-12-30 13:37:05 +0100

    proberen info op te halen
    proberen om vanaf de contact pagina van de pxl de basis info op te halen

--- gen/com/mobsoft/pxlapp/R.java
+++ gen/com/mobsoft/pxlapp/R.java
@@ -24,12 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int button_info=0x7f080004;
-        public static final int button_lesroosters=0x7f080003;
-        public static final int button_menu=0x7f080002;
-        public static final int gekozen_klas_string=0x7f080000;
-        public static final int lesrooster_weergeven_button=0x7f080001;
+        public static final int action_settings=0x7f080006;
+        public static final int button_info=0x7f080005;
+        public static final int button_lesroosters=0x7f080004;
+        public static final int button_menu=0x7f080003;
+        public static final int gekozen_klas_string=0x7f080001;
+        public static final int lesrooster_weergeven_button=0x7f080002;
+        public static final int webview=0x7f080000;
     }
     public static final class layout {
         public static final int activity_info=0x7f030000;
@@ -52,6 +53,7 @@ public final class R {
         public static final int button_menu=0x7f050003;
         public static final int geef_klas_message=0x7f05000a;
         public static final int hello_world=0x7f050002;
+        public static final int info_message=0x7f05000b;
         public static final int title_activity_info=0x7f050009;
         public static final int title_activity_lesroosters=0x7f050008;
         public static final int title_activity_menu=0x7f050007;
