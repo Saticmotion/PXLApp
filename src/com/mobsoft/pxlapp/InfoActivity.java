b39f508ffb2e625e6a3c4f77d4b4b107be5b0c59
lambrix <lambrix_s@hotmail.com>
2014-01-09 14:03:30 +0100

    Bla

--- src/com/mobsoft/pxlapp/InfoActivity.java
+++ src/com/mobsoft/pxlapp/InfoActivity.java
@@ -1,9 +1,13 @@
 package com.mobsoft.pxlapp;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.support.v4.app.NavUtils;
 import android.text.Html;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,7 +21,7 @@ public class InfoActivity extends Activity {
 	{
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_info);	
-			
+			setupActionBar();
 			
 			ImageView im = (ImageView) findViewById(R.id.imageView1);  
 		    im.setImageResource(R.drawable.pxl_logo_noborder);
@@ -50,6 +54,30 @@ public class InfoActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.info, menu);
 		return true;
+	}
+	
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
