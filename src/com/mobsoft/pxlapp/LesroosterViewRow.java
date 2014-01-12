e9a0f0795b6f87a84d45ede06599cd1f3a9e4362
Saticmotion <simon_verbeke_@hotmail.com>
2014-01-12 23:23:00 +0000

    Eerste poging GUI lessenrooster

---
+++ src/com/mobsoft/pxlapp/LesroosterViewRow.java
@@ -0,0 +1,27 @@
+package com.mobsoft.pxlapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ListView;
+
+public class LesroosterViewRow extends Activity 
+{
+	private ListView listView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lesrooster_view_row);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.lesrooster_view_row, menu);
+		return true;
+	}
+
+}
