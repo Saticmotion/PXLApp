54cbf93686a59d4c4096a28324d7be6acc576c47
Saticmotion <simon_verbeke_@hotmail.com>
2014-01-14 16:34:17 +0000

    navbar toegevoegd in lessenroosters

--- src/com/mobsoft/pxlapp/LesroostersActivity.java
+++ src/com/mobsoft/pxlapp/LesroostersActivity.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ExecutionException;
 import com.mobsoft.pxlapp.util.LogUtil;
 import com.mobsoft.pxlapp.util.SimpleDateTime;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
@@ -14,9 +15,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -39,13 +43,44 @@ public class LesroostersActivity extends Activity
 		return true;
 	}
 
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar()
+	{
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+		{
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case android.R.id.home:
+				// This ID represents the Home or Up button. In the case of this
+				// activity, the Up button is shown. Use NavUtils to allow users
+				// to navigate up one level in the application structure. For
+				// more details, see the Navigation pattern on Android Design:
+				//
+				// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+				//
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
 	public void displayLesrooster(View view)
 	{
 		SimpleDateTime test = new SimpleDateTime();
 		Log.d(LogUtil.PXL_TAG, " " + test.getJaar());
 		
 		TextView klasText = (TextView) findViewById(R.id.gekozen_klas_string);
-		String klas = klasText.getText().toString().toUpperCase().replace("\n ", ""); //formatteer klas volgens voorbeeld: 2TING.
+		String klas = klasText.getText().toString().toUpperCase().replace(" ", ""); //formatteer klas volgens voorbeeld: 2TING.
 		Log.d("Pxl App", klas);
 		
 		try 
