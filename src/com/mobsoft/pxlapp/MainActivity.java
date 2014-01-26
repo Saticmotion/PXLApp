7110b268e8a82a3c551a1b786ced14e182bd79c3
Saticmotion <simon_verbeke_@hotmail.com>
2014-01-26 17:22:53 +0100

    Icoontje gemaakt voor kalender

--- src/com/mobsoft/pxlapp/MainActivity.java
+++ src/com/mobsoft/pxlapp/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends Activity
 	    AdView adView = (AdView)this.findViewById(R.id.adView);
 	    AdRequest adRequest = new AdRequest.Builder().build();
 	    adView.loadAd(adRequest);
-
 	}
 
 	@Override
